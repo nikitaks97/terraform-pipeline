3db19c1e52e3e763616808967ffe5fb8e204ea1b
nikitaks97 <nikitaks997@gmail.com>
2025-06-08 15:55:16 +0200

    fix: remove unused output variables from VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -64,15 +64,3 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.rtb.id
 }
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
