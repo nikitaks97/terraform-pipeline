910a8ace60d0b65c45eab4766090c4f9ce2daabf
nikitaks97 <nikitaks997@gmail.com>
2025-06-08 16:01:11 +0200

    style: format code for consistency in autoscaling module

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,30 +1,30 @@
 resource "aws_launch_template" "web" {
-  name_prefix   = var.lt_name_prefix
-  image_id      = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  vpc_security_group_ids = var.security_group_ids
- # subnet_id     = var.subnet_id
+  name_prefix             = var.lt_name_prefix
+  image_id                = var.ami
+  instance_type           = var.instance_type
+  key_name                = var.key_name
+  vpc_security_group_ids  = var.security_group_ids
+ # subnet_id             = var.subnet_id
   metadata_options {
     http_tokens = "required"
   }
   tag_specifications {
     resource_type = "instance"
     tags = merge({
-      Name = var.lt_name_prefix,
+      Name        = var.lt_name_prefix,
       environment = var.environment,
-      owner = var.owner,
+      owner       = var.owner,
       cost_center = var.cost_center
     }, var.extra_tags)
   }
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name                      = var.asg_name
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  desired_capacity          = var.desired_capacity
-  vpc_zone_identifier       = var.subnets
+  name                = var.asg_name
+  max_size            = var.max_size
+  min_size            = var.min_size
+  desired_capacity    = var.desired_capacity
+  vpc_zone_identifier = var.subnets
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
