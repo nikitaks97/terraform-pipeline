bf6b26e6e197711abe2710e938d1bd1556ddbb76
nikitaks97 <nikitaks997@gmail.com>
2025-06-08 16:53:51 +0200

    fix: update Terraform workflow and backend configuration for improved state management

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0"
   backend "s3" {
-    bucket  = "statefile-0"
-    key     = "statefile-0/s3-tf-new"
-    region  = "us-east-1"
-    encrypt = true
-    # dynamodb_table = "terraform-locks"   # Optional for state locking
+    bucket         = "statefile-0"
+    key            = "statefile-0/s3-tf-new"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"   # For state locking
   }
 }
