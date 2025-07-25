980392024f8e914a0c61b7501c4989f5ca9191e3
nikitaks97 <nikitaks997@gmail.com>
2025-07-25 14:05:06 +0200

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.0"
   backend "s3" {
-    bucket         = "statefile-0"
-    key            = "statefile-0/s3-tf-new"
+    bucket         = "statebucket-0"
+    key            = "statebucket-0/s3-tf-new"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-locks"  # Uncomment and specify DynamoDB table name for state locking
